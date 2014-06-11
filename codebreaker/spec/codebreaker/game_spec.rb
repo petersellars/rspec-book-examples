56fe9dffde0c236bb2f2bf50858b838e1a7b0069
petersellars <psellars@gmail.com>
2014-06-11 09:30:09 +0000

    Added placement for marks and specification for it

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -42,6 +42,14 @@ module Codebreaker
         end
       end 
 
+      context "with 1 number match and 1 exact match (in that order)" do
+        it "sends a mark with '+-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+-')
+          game.guess('2535')
+        end
+      end
+
       context "with 2 number matches" do
         it "sends a mark with '--'" do
           game.start('1234')
